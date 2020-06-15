e9371100cdcfefb44d341dce2d85c27b3ac98b16
Lisa Zapson <lisazapson@ymail.com>
2020-06-14 20:19:01 -0400

    client-side ajax implementation

--- index.js
+++ index.js
@@ -1,46 +1,121 @@
+const fs = require('fs')
 const url = require('url')
 const express = require('express')
 const axios = require('axios')
+const WebSocket = require('ws')
 
-async function getStock(symbol) {
-        const response = await axios.get(`https://api.tiingo.com/tiingo/daily/${symbol}/prices`, {
-        'headers': {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token 4f329d3ebe77b1634915729f96b1f9b244f318ae'
-        }
-    })
-    return response.data
-}
+
+/**
+ * Web Socket - get Stock prices using finnhub.io - Stream real-time trades for US stocks, forex and crypto. 
+ */
+
+// var ws = new WebSocket('wss://api.tiingo.com/iex');
+
+// var subscribe = {
+//     'eventName':'subscribe',
+//     'authorization': '4f329d3ebe77b1634915729f96b1f9b244f318ae',
+//     'eventData': {
+//         'thresholdLevel': 5,
+//         'tickers': ['spy']
+//     }
+// }
+// ws.on('open', function open() {
+//     ws.send(JSON.stringify(subscribe));
+// });
+
+// ws.on('message', function(data, flags) {
+//     console.log(data)
+// });
+
+
+// const socket = new WebSocket('wss://ws.finnhub.io?token=brig0b7rh5rf00gkcetg');
+
+// // Connection opened -> Subscribe
+// socket.addEventListener('open', function (event) {
+//     socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+//     // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'C'}))
+//     // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BAC'}))
+//     // socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'JPM'}))
+// });
+
+// // Listen for messages
+// socket.addEventListener('message', function (event) {
+//     console.log('Message from server ', event.data);
+// });
+
+// // Unsubscribe
+//  var unsubscribe = function(symbol) {
+//     socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+// }
+
+
+/**
+ * Express Server
+ */
 
 const app = express();
 
+// Express server access to static HTML 
 app.use(express.static('public'))
 
-// Server
+// Dynamic HTML using templates
+let tickerHtml = fs.readFileSync(`${__dirname}/templates/ticker.html`, 'utf-8')
+tickerHtml = tickerHtml.replace('{%SYMBOL%}', "BAC")
 
-app.get('/', (req, res) => {
+
+/**
+ * API - get Stock prices using api.tiingo.com - A Reliable, Enterprise-Grade Financial Markets API
+ */
+
+
+async function getStock(symbol) {
+    try {
+        const response = await axios.get(`https://api.tiingo.com/tiingo/daily/${symbol}/prices`, {
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token 4f329d3ebe77b1634915729f96b1f9b244f318ae'
+            }
+        })
+        return response.data
+
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+
+
+}
+
+
+
+/**
+ * Server Routes
+ */
+
+app.get('/websocket', (req, res) => {
     res
         .status(200)
         .json({
-            "message": "Hello from the server"
+            "tickerHtml": tickerHtml
         })
 })
 
-app.get('/stocks', (req, res) => {
-    const {pathname, query} = url.parse(req.url, true);
+app.get('/api', (req, res) => {
+    const {
+        query
+    } = url.parse(req.url, true);
     getStock(query.symbol).then(data => {
         res
-        .status(200)
-        .json({
-            "data": data
-        })
+            .status(200)
+            .json({
+                "data": data
+            })
     })
 })
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint')
-})
 
+// run server
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
-
 var stockUpdaterModel = (function () {
 
-    const StocksAndPrices = new Map();
+    const stocksAndPrices = new Map();
 
     const stockList = [{
             name: "Wells Fargo & Co",
@@ -23,38 +22,73 @@ var stockUpdaterModel = (function () {
 
     class Stock {
         isDirty = false;
-        price = null;
+        price = {};
         constructor(name, symbol) {
             this.name = name;
             this.symbol = symbol;
         }
-
         setDirty(isDirty) {
             this.isDirty = isDirty;
         }
-
     }
 
-
-
-    init = function () {
-        stockList.forEach(stock => {
-            const newStock = new Stock(stock.name, stock.symbol);
-            StocksAndPrices.set(stock.symbol, newStock);
+    const init = function () {
+        stockList.forEach(({
+            name,
+            symbol
+        }) => {
+            const newStock = new Stock(name, symbol);
+            stocksAndPrices.set(symbol, newStock);
         })
-        refreshStockPrices();
     }
 
-    function refreshStockPrices() {
-        StocksAndPrices.forEach((value, key) => {
-            fetch(`/stocks/?symbol=${key}`).then(response => console.log(response.json()))
+    const getStockList = function () {
+        return stockList;
+    }
+
+    const setStockPrice = function (symbol, price) {
+        const stock = stocksAndPrices.get(symbol);
+        if (stock && (stock.price.low !== price.low || stock.price.high !== price.high)) {
+            stock.price = {
+                ...price
+            };
+            stock.isDirty = true;
+        }
+    }
+
+    const getDirtyStocks = function () {
+        const dirtyList = [];
+        stocksAndPrices.forEach(({
+            isDirty,
+            price
+        }, key) => {
+            if (isDirty) {
+                isDirty = false;
+                dirtyList.push({
+                    symbol: key,
+                    price
+                })
+            }
         })
+        return dirtyList;
+    }
+
+    const refreshStockPrice = async function (symbol) {
+        try {
+            const response = await fetch(`/api/?symbol=${symbol}`)
+            return response.json();
+        } catch (error) {
+
+        }
+
     }
 
     return {
-        init: function () {
-            init();
-        }
+        init: init,
+        getStockList: getStockList,
+        refreshStockPrice: refreshStockPrice,
+        setStockPrice: setStockPrice,
+        getDirtyStocks: getDirtyStocks
     }
 
 })();
@@ -62,29 +96,93 @@ var stockUpdaterModel = (function () {
 
 var stockUpdaterView = (function () {
 
+    const DOMstrings = {
+        STOCK_LIST: "stock__list",
+        STOCK_LIST_ITEM: "stock__list--item",
+        STOCK_NAME: "stock__name",
+        STOCK_SYMBOL: "stock__symbol",
+        STOCK_PRICE: "stock__price",
+        STOCK_PRICE_TEXT: "stock__price--text",
+        STOCK_PRICE_OPEN: "stock__price--open",
+        STOCK_PRICE_CLOSE: "stock__price--close",
+        STOCK_PRICE_LOW: "stock__price--low",
+        STOCK_PRICE_HIGH: "stock__price--high"
+
+    }
+
+    const addStockListItem = function (name, symbol) {
+        const listItem = document.createElement("LI");
+        listItem.className = `${DOMstrings.STOCK_LIST_ITEM} item__${symbol}`;
+        const innerHtml = `
+        <span class=${DOMstrings.STOCK_SYMBOL}>${symbol}</span>
+        <span class=${DOMstrings.STOCK_PRICE_TEXT}>open</span>
+        <span class="${DOMstrings.STOCK_PRICE} ${DOMstrings.STOCK_PRICE_OPEN}"></span>
+        <span class=${DOMstrings.STOCK_PRICE_TEXT}>close</span>
+        <span class="${DOMstrings.STOCK_PRICE} ${DOMstrings.STOCK_PRICE_CLOSE}"></span>
+        <span class=${DOMstrings.STOCK_PRICE_TEXT}>low</span>
+        <span class="${DOMstrings.STOCK_PRICE} ${DOMstrings.STOCK_PRICE_LOW}"></span>
+        <span class=${DOMstrings.STOCK_PRICE_TEXT}>high</span>
+        <span class="${DOMstrings.STOCK_PRICE} ${DOMstrings.STOCK_PRICE_HIGH}"></span>
+        `;
+        listItem.innerHTML = innerHtml;
+        document.querySelector(`.${DOMstrings.STOCK_LIST}`).appendChild(listItem);
+    }
+
+    const renderPrice = function (symbol, price) {
+        const item = document.querySelector(`.item__${symbol}`);
+        item.querySelector(`.${DOMstrings.STOCK_PRICE_OPEN}`).textContent = price.open.toFixed(2);
+        item.querySelector(`.${DOMstrings.STOCK_PRICE_CLOSE}`).textContent = price.close.toFixed(2);
+        item.querySelector(`.${DOMstrings.STOCK_PRICE_LOW}`).textContent = price.low.toFixed(2);
+        item.querySelector(`.${DOMstrings.STOCK_PRICE_HIGH}`).textContent = price.high.toFixed(2);
+    }
+
+    return {
+        addStockListItem: addStockListItem,
+        renderPrice: renderPrice
+    }
+
 })()
 
 
 var stockUpdaterController = (function (model, view) {
-
-
     refreshStockPrices = function () {
-
+        model.getStockList().forEach(({
+            symbol
+        }) => {
+            model.refreshStockPrice(symbol)
+                .then(stockPrices => model.setStockPrice(symbol, stockPrices.data[0]))
+                .catch(error => console.log(error))
+        })
     }
 
-    setupListeners = function () {
-        document.querySelector("#refresh-button").addEventListener('click', refreshStockPrices)
+    updateUI = function () {
+        setTimeout(() => {
+            model.getDirtyStocks().forEach(({
+                symbol,
+                price
+            }) => {
+                view.renderPrice(symbol, price);
+            })
+
+        }, 2000)
+    }
+
+    const init = function () {
+        model.init();
+        model.getStockList().forEach(({
+            name,
+            symbol
+        }) => view.addStockListItem(name, symbol))
+        refreshStockPrices();
+        updateUI();
     }
 
 
     return {
-        init: function () {
-            model.init();
-        }
+        init: init
     }
 
 
 })(stockUpdaterModel, stockUpdaterView);
 
 stockUpdaterController.init();
-
